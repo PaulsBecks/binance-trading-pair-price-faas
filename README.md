42abf0608c136c1279bbedd2ad2cf17a70938130
Paul Beck <paulbeck@Pauls-MBP.fritz.box>
2020-12-31 17:11:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Binance Trading Pair Price Function
+
+This function calls the binance API for the price of a given trading pair. 
+
+It will then slice the number and return the first 6 digits.
+
+You can configur the trading pair by providing a environment variable `TADING_PAIR`.
---
+++ index.js
@@ -0,0 +1,45 @@
+var https = require('https');
+
+const TRADING_PAIR = process.env.TRADING_PAIR || "BTCUSDT"
+
+const options = {
+    host: "api.binance.com",
+    port: "443",
+    path: '/api/v3/ticker/price?symbol='+TRADING_PAIR,
+    method: 'GET',
+};
+
+function doRequest(options) {
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+        res.setEncoding('utf8');
+        let responseBody = '';
+
+        res.on('data', (chunk) => {
+            responseBody += chunk;
+        });
+
+        res.on('end', () => {
+            resolve(JSON.parse(responseBody));
+        });
+        });
+
+        req.on('error', (err) => {
+        reject(err);
+        });
+
+        req.end();
+    });
+}
+
+const main = async (req, res) => {
+    const data = await doRequest(options)
+    const text = (data.price+"").slice(0,6)
+    const symbol = []
+    return res.send({text, symbol})
+}
+
+main()
+
+module.exports = main
+
