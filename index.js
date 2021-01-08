b260e437a6ab616ab4b8dc178a2cc6cde12d98f4
Paul Beck <paulbeck@Pauls-MBP.fritz.box>
2021-01-08 19:48:42 +0100

    fix json response

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ const TRADING_PAIR = process.env.TRADING_PAIR || "BTCUSDT"
 const options = {
     host: "api.binance.com",
     port: "443",
-    path: '/api/v3/ticker/price?symbol='+TRADING_PAIR,
+    path: '/api/v3/ticker/price?symbol=' + TRADING_PAIR,
     method: 'GET',
 };
 
 function doRequest(options) {
     return new Promise((resolve, reject) => {
         const req = https.request(options, (res) => {
-        res.setEncoding('utf8');
-        let responseBody = '';
+            res.setEncoding('utf8');
+            let responseBody = '';
 
-        res.on('data', (chunk) => {
-            responseBody += chunk;
-        });
+            res.on('data', (chunk) => {
+                responseBody += chunk;
+            });
 
-        res.on('end', () => {
-            resolve(JSON.parse(responseBody));
-        });
+            res.on('end', () => {
+                resolve(JSON.parse(responseBody));
+            });
         });
 
         req.on('error', (err) => {
-        reject(err);
+            reject(err);
         });
 
         req.end();
@@ -34,9 +34,9 @@ function doRequest(options) {
 
 const main = async (req, res) => {
     const data = await doRequest(options)
-    const text = (data.price+"").slice(0,6)
+    const text = (data.price + "").slice(0, 6)
     const symbol = []
-    return res.send({text, symbol})
+    return res.json({ text, symbol })
 }
 
 main()
